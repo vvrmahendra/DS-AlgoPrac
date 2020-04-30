a9508a71ae57b1dadec620183798e12e8441b5e4
vvrmahendra <vvrmahendra@gmail.com>
2020-04-30 15:36:36 +0530

    rename of a folder

---
+++ binaryTree/validSequence.py
@@ -0,0 +1,67 @@
+"""  Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree
+Given a binary tree where each path going from the root to any leaf form a valid sequence, check if a given string is a valid sequence in such binary tree. 
+
+We get the given string from the concatenation of an array of integers arr and the concatenation of all values of the nodes along a path results in a sequence in the given binary tree.
+
+ 
+
+Example 1:
+
+
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,0,1]
+Output: true
+Explanation: 
+The path 0 -> 1 -> 0 -> 1 is a valid sequence (green color in the figure). 
+Other valid sequences are: 
+0 -> 1 -> 1 -> 0 
+0 -> 0 -> 0
+Example 2:
+
+
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,0,1]
+Output: false 
+Explanation: The path 0 -> 0 -> 1 does not exist, therefore it is not even a sequence.
+Example 3:
+
+
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,1]
+Output: false
+Explanation: The path 0 -> 1 -> 1 is a sequence, but it is not a valid sequence.
+ 
+
+Constraints:
+
+1 <= arr.length <= 5000
+0 <= arr[i] <= 9
+Each node's value is between [0 - 9]."""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidSequence(self, root: TreeNode, arr: List[int]) -> bool:
+        seq = arr
+        def helper(head, ind):
+            if ind == len(seq)-1:
+                if head and head.val == seq[ind] and head.left is None and head.right is None:
+                    return True
+                else:
+                    return False
+
+            if not head:
+                return False
+
+            if head.val != seq[ind]:
+                return False
+
+            left = helper(head.left, ind+1)
+            right = helper(head.right, ind+1)
+            return left or right
+        
+        return helper(root, 0)
