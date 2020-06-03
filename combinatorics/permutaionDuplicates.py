ab7f79419d1a536ab72f6101e45bbed9764dcc17
vvrmahendra <vvrmahendra@gmail.com>
2020-06-03 19:13:41 +0530

    Create permutaionDuplicates.py

---
+++ combinatorics/permutaionDuplicates.py
@@ -0,0 +1,21 @@
+def permutationWithDuplicates(A):
+    if not A: return []
+    ans = []
+    def helper(A, l, r):
+        if l == r:
+            ans.append([i for i in A])
+            return
+        
+        for i in range(l,r+1):
+            if A[i] not in A[l:i]:
+                A[i],A[l] = A[l],A[i]
+                helper(A, l+1, r)
+                A[i],A[l] = A[l],A[i]
+            
+        return
+    
+    helper(A,0,len(A)-1)
+    return ans
+    
+permutationWithDuplicates([1,1,3])
+        
